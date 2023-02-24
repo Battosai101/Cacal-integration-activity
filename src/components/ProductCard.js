afb8baa65435b1855846c13fdbfd5814117e8bf9
Battosai101 <cyrruzzc@gmail.com>
2023-02-24 19:27:12 +0800

    Done 02/24/23

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,7 +2,7 @@ import star from '../star.png'
 export default function ProductCard(props){
     return(
         <button className='relative w-[516px] h-[291px] shrink-0 rounded-[21px] overflow-hidden snap-start hover:scale-90 hover:shadow-[0_4px_12px_-1px_rgba(196,196,196,0.4)] hover:duration-300 hover:ease-out'>
-                <img src={props.img} className='w-full h-full'/>
+                <img src={props.img} alt="" className='w-full h-full'/>
                 <div className='absolute top-0 w-full h-full bg-gradient-to-b from-transparent to-black'></div>
                 <div className='absolute w-full flex flex-col pl-[33px] top-[223px] bottom-5 text-white'>
                     <div className='flex justify-between items-center font-[Outfit] font-medium text-2xl'>
