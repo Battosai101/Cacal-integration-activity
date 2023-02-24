07b8a23205230190bd532c9b10b2234d9c0d557e
Battosai101 <cyrruzzc@gmail.com>
2023-02-24 18:09:28 +0800

    simple app done with api integration

--- src/App.js
+++ src/App.js
@@ -26,13 +26,22 @@ export default function App() {
               id={item.id}
               title={item.title}
               price={item.price}
+              stock={item.stock}
+              rate={item.rating}
+              img={item.thumbnail}
+              cat={item.category}
+              brand={item.brand}
           />
   })
 
   return (
-    <div>
+    <div className='relative font-[Outfit] text-white w-full h-screen border-2 border-white'>
+      <h1 className='text-white font-bold text-3xl tracking-wider mt-[50px] ml-[30px] border-2 border-white'>TechBinge</h1>
+      <h1 className='text-white font-bold text-2xl tracking-wider mt-[40px] ml-[30px] text-center underline underline-offset-8 border-2 border-white'>Products</h1>
+      <div className='text-white flex items-center mt-[30px] px-[30px] space-x-4 py-2 overflow-auto scrollbar-hide scroll-smooth scroll-pl-[30px] snap-x'>
+        {items}
+      </div>
       {error!=="" && error}
-      {items}
     </div>
   );
 }
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,6 +1,35 @@
-
+import star from '../star.png'
 export default function ProductCard(props){
     return(
-        <h3>{props.id} {props.title} - ${props.price}</h3>
+        <button className='relative w-[516px] h-[291px] shrink-0 rounded-[21px] overflow-hidden snap-start hover:scale-90 hover:shadow-[0_4px_12px_-1px_rgba(196,196,196,0.4)] hover:duration-300 hover:ease-out'>
+                <img src={props.img} className='w-full h-full'/>
+                <div className='absolute top-0 w-full h-full bg-gradient-to-b from-transparent to-black'></div>
+                <div className='absolute w-full flex flex-col pl-[33px] top-[223px] bottom-5 text-white'>
+                    <div className='flex justify-between items-center font-[Outfit] font-medium text-2xl'>
+                        <h1 className=' flex justify-start items-center font-[Outfit] font-medium text-2xl'>
+                            {props.title}
+                        </h1>
+                        <h1 className=' flex justify-end items-center font-[Outfit] font-medium text-sm mr-7'>
+                            $ {props.price}
+                        </h1>
+                    </div>
+                    <div className='flex items-center justify-between'>
+                        <p className='font-[Outfit] font-normal text-xs text-[#747474]'>
+                            Category: {props.cat}
+                        </p>
+                        <div className='flex items-center mr-8'>
+                            <img src={star} alt="star icon" className='w-[19.97px] h-[18.99px] mr-1' />
+                            <p className='font-[Outfit] font-medium text-xs'>
+                                {props.rate}
+                            </p>
+                        </div>
+                    </div>
+                    <div className='flex items-center justify-between'>
+                        <p className='font-[Outfit] font-normal text-xs text-[#747474]'>
+                            Brand: {props.brand}
+                        </p>
+                    </div>
+                </div>
+           </button>
     )
 }
